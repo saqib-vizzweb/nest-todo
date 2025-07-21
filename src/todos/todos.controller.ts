c843c10804c3b3be0c91cb909f0664f6a3acd0f4
saqib-vizzweb <saqibaziz@vizzweb.com>
2025-07-21 16:14:51 +0500

    exception filter

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -7,11 +7,14 @@ import {
   Param,
   Delete,
   ParseIntPipe,
+  UseFilters,
 } from '@nestjs/common';
 import { TodosService } from './todos.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { HttpExceptionFilter } from 'src/common/filters/http-exception.filter';
 
+@UseFilters(new HttpExceptionFilter())
 @Controller('todos')
 export class TodosController {
   constructor(private readonly todosService: TodosService) {}
